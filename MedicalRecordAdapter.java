ece4d1eec7bd89bbc28eb52ee42d11fe020de82f
jackhempstead <jackhemp01@gmail.com>
2021-12-05 19:10:19 -0500

    first committ

--- MedicalRecordAdapter.java
+++ MedicalRecordAdapter.java
@@ -1,3 +1,39 @@
-public class MedicalRecordAdapter {
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * BY Jack Hempstead 
+ */
+public class MedicalRecordAdapter implements MedicalRecord {
+    private HealthRecord record;
     
+public MedicalRecordAdapter(HealthRecord record) {
+    this.record = record;
+}
+public String getFirstName() {
+    return record.getName();
+}
+public String getLastName() {
+    return null;
+}
+
+public Date getBirthday() {
+    return record.getBirthdate();
+}
+public Gender getGender() {
+    Gender token; 
+    token = record.getGender().toString();
+    return token;
+}
+
+public void addVisit(Date date, boolean well, String description) {
+    
+}
+
+public ArrayList<Visit> getVisitHistory() {
+    return null;
+}
+public String toString() {
+    return null;
+}
 }
